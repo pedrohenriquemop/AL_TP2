17c33fe03d7175b37cc1972d42fe7909cee3f453
pedrohenriquemop <madeirapedrohenrique1112@gmail.com>
2023-10-22 21:52:12 -0300

    optimized

--- main.cpp
+++ main.cpp
@@ -3,244 +3,121 @@
 #include <algorithm>
 #include <iostream>
 #include <set>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
 #define DEBUG false
 
-class Aresta {
+class Edge {
    public:
-    int v1, v2, ano, tempo, custo;
+    int v, ano, tempo, custo;
 
-    Aresta() : Aresta(-1, -1, 0, 0, 0){};
-
-    Aresta(int _v1, int _v2, int _ano, int _tempo, int _custo) : v1(_v1), v2(_v2), ano(_ano), tempo(_tempo), custo(_custo){};
+    Edge(int _v, int _ano, int _tempo, int _custo) : v(_v), ano(_ano), tempo(_tempo), custo(_custo){};
 };
 
-int findMinDistanceIndexTempo(int size, vector<long> &dist, vector<bool> &visited) {
-    long minDist = 99999999999, minIndex = -1;
-
-    for (int i = 0; i < size; i++) {
-        if (!visited[i] && dist[i] < minDist) {
-            minDist = dist[i];
-            minIndex = i;
-        }
-    }
-
-    return minIndex;
-}
-
-vector<int> findAdjacentVertices(int src, vector<Aresta> &arestas) {
-    vector<int> adjacentVertices;
-
-    for (int i = 0; i < arestas.size(); i++) {
-        if (arestas[i].v1 == src) {
-            adjacentVertices.push_back(arestas[i].v2);
-        } else if (arestas[i].v2 == src) {
-            adjacentVertices.push_back(arestas[i].v1);
-        }
-    }
-
-    return adjacentVertices;
-}
-
-Aresta findAresta(int v1, int v2, vector<Aresta> &arestas) {
-    for (int i = 0; i < arestas.size(); i++) {
-        if (arestas[i].v1 == v1 && arestas[i].v2 == v2 || arestas[i].v1 == v2 && arestas[i].v2 == v1) {
-            return arestas[i];
-        }
-    }
-
-    return Aresta(v1, v2, 0, 0, 0);
-}
-
-void dijkstra(int src, int size, vector<Aresta> &arestas) {
-    vector<long> dist(size, 99999999999);
-    vector<bool> visited(size, false);
+void dijkstra(int src, int size, vector<vector<Edge>> &adj) {
+    set<pair<int, int>> s;  // guarda pares {tempo, verticeAdj}
+    vector<long> dist(size, 1215752191);
     vector<int> anos(size, 0);
 
+    s.insert({0, src});
     dist[src] = 0;
-    for (int i = 0; i < size; i++) {
-        int u = findMinDistanceIndexTempo(size, dist, visited);
-        visited[u] = true;
 
-        vector<int> adjacentVertices = findAdjacentVertices(u, arestas);
+    while (!s.empty()) {
+        pair<int, int> curr = *(s.begin());
 
-        for (int v : adjacentVertices) {
-            Aresta a = findAresta(u, v, arestas);
-            if (!visited[v] && dist[u] + a.tempo < dist[v]) {
-                dist[v] = dist[u] + a.tempo;
-                anos[v] = a.ano;
+        int currTempo = curr.first;
+        int currVertice = curr.second;
+        s.erase(curr);
+
+        for (Edge edge : adj[currVertice]) {
+            int verticeAdj = edge.v;
+            int verticeTempo = edge.tempo;
+
+            if (currTempo + verticeTempo < dist[verticeAdj]) {
+                if (dist[verticeAdj] != 1215752191) {
+                    s.erase({dist[verticeAdj], verticeAdj});
+                }
+
+                dist[verticeAdj] = currTempo + verticeTempo;
+                s.insert({dist[verticeAdj], verticeAdj});
+
+                anos[verticeAdj] = edge.ano;
             }
         }
     }
 
-    if (DEBUG) {
-        for (int i = 0; i < dist.size(); i++)
-            cout << "dist[" << i << "]: " << dist[i] << endl;
-
-        cout << "Ano max: " << *max_element(anos.begin(), anos.end()) << endl;
-    } else {
-        for (int d : dist)
-            cout << d << endl;
-        cout << *max_element(anos.begin(), anos.end()) << endl;
-    }
+    for (int d : dist)
+        cout << d << endl;
+    cout << *max_element(anos.begin(), anos.end()) << endl;
 }
 
-set<int> findAdjacentEdgesIds(int src, vector<Aresta> &arestas) {
-    set<int> adj;
-
-    for (int i = 0; i < arestas.size(); i++) {
-        if (arestas[i].v1 == src || arestas[i].v2 == src) {
-            adj.insert(i);
-        }
-    }
-
-    return adj;
-}
-
-int findMinCustoArestaId(vector<Aresta> &arestas, set<int> &arestasIds) {
-    int menor = 99999, menorId = -1;
-
-    for (int id : arestasIds) {
-        if (arestas[id].custo < menor) {
-            menor = arestas[id].custo;
-            menorId = id;
-        }
-    }
-
-    return menorId;
-}
-
-int findMinAnoArestaId(vector<Aresta> &arestas, set<int> &arestasIds) {
-    int menor = 99999999, menorId = -1;
-
-    for (int id : arestasIds) {
-        if (arestas[id].ano < menor) {
-            menor = arestas[id].ano;
-            menorId = id;
-        }
-    }
-
-    return menorId;
-}
-
-bool contains(vector<int> &v, int x) {
-    return std::find(v.begin(), v.end(), x) != v.end();
-}
-
-void prim(int src, int size, vector<Aresta> &arestas) {
-    vector<Aresta> mst;
-    long custoTotal = 0;
-    vector<int> visited(size);
-    visited.push_back(src);
-
-    set<int> arestasIdsToPick;
-
-    set<int> foundResult = findAdjacentEdgesIds(src, arestas);
-    arestasIdsToPick.insert(foundResult.cbegin(), foundResult.cend());
-
-    while (!arestasIdsToPick.empty()) {
-        int minArestaId = findMinCustoArestaId(arestas, arestasIdsToPick);
-        arestasIdsToPick.erase(minArestaId);
-
-        Aresta currAresta = arestas[minArestaId];
-
-        // if (DEBUG) {
-        //     cout << "Prim visitando aresta: " << currAresta.v1 << " - " << currAresta.v2 << endl;
-        //     cout << "Estado do set de arestas: " << endl;
-        //     for (int s : arestasIdsToPick) {
-        //         cout << s << " ";
-        //     }
-        //     cout << endl;
-        // }
-
-        if (!contains(visited, currAresta.v1)) {
-            // if (DEBUG) {
-            //     cout << "Entrou 1" << endl;
-            // }
-            mst.push_back(currAresta);
-            visited.push_back(currAresta.v1);
-
-            custoTotal += currAresta.custo;
-
-            foundResult = findAdjacentEdgesIds(currAresta.v1, arestas);
-            arestasIdsToPick.insert(foundResult.cbegin(), foundResult.cend());
-        }
-        if (!contains(visited, currAresta.v2)) {
-            // if (DEBUG) {
-            //     cout << "Entrou 2" << endl;
-            // }
-            mst.push_back(currAresta);
-            visited.push_back(currAresta.v2);
-
-            custoTotal += currAresta.custo;
-
-            foundResult = findAdjacentEdgesIds(currAresta.v2, arestas);
-            arestasIdsToPick.insert(foundResult.cbegin(), foundResult.cend());
-        }
-    }
-
-    if (DEBUG) {
-        cout << "Arestas Prim ( " << custoTotal << " ):" << endl;
-        for (Aresta a : mst) {
-            cout << a.v1 << " - " << a.v2 << ": " << a.custo << endl;
-        }
-    } else {
-        cout << custoTotal << endl;
-    }
-}
-
-void primAno(int src, int size, vector<Aresta> &arestas) {
-    vector<Aresta> mst;
+void primAno(int src, int size, vector<vector<Edge>> &adj) {
+    set<pair<int, int>> s;  // guarda pares {ano, verticeAdj}
+    vector<bool> visited(size, false);
     int anoMax = 0;
-    vector<int> visited(size);
-    visited.push_back(src);
 
-    set<int> arestasIdsToPick;
+    s.insert({0, src});
 
-    set<int> foundResult = findAdjacentEdgesIds(src, arestas);
-    arestasIdsToPick.insert(foundResult.cbegin(), foundResult.cend());
+    while (!s.empty()) {
+        pair<int, int> curr = *(s.begin());
+        s.erase(curr);
 
-    while (!arestasIdsToPick.empty()) {
-        int minArestaId = findMinAnoArestaId(arestas, arestasIdsToPick);
-        arestasIdsToPick.erase(minArestaId);
+        int currAno = curr.first;
+        int currVertice = curr.second;
 
-        Aresta currAresta = arestas[minArestaId];
+        if (visited[currVertice]) continue;
 
-        if (!contains(visited, currAresta.v1)) {
-            mst.push_back(currAresta);
-            visited.push_back(currAresta.v1);
+        visited[currVertice] = true;
+        if (currAno > anoMax) anoMax = currAno;
 
-            if (currAresta.ano > anoMax) anoMax = currAresta.ano;
-
-            foundResult = findAdjacentEdgesIds(currAresta.v1, arestas);
-            arestasIdsToPick.insert(foundResult.cbegin(), foundResult.cend());
-        }
-        if (!contains(visited, currAresta.v2)) {
-            mst.push_back(currAresta);
-            visited.push_back(currAresta.v2);
-
-            if (currAresta.ano > anoMax) anoMax = currAresta.ano;
-
-            foundResult = findAdjacentEdgesIds(currAresta.v2, arestas);
-            arestasIdsToPick.insert(foundResult.cbegin(), foundResult.cend());
+        for (Edge edge : adj[currVertice]) {
+            if (!visited[edge.v]) {
+                s.insert({edge.ano, edge.v});
+            }
         }
     }
 
-    if (DEBUG) {
-        cout << "Arestas Prim Ano ( " << anoMax << " ):" << endl;
-    } else {
-        cout << anoMax << endl;
+    cout << anoMax << endl;
+}
+
+void primCusto(int src, int size, vector<vector<Edge>> &adj) {
+    set<pair<int, int>> s;  // guarda pares {custo, verticeAdj}
+    vector<bool> visited(size, false);
+    long costSum = 0;
+
+    s.insert({0, src});
+
+    while (!s.empty()) {
+        pair<int, int> curr = *(s.begin());
+        s.erase(curr);
+
+        int currVertice = curr.second;
+        int currCusto = curr.first;
+
+        if (visited[currVertice]) continue;
+
+        visited[currVertice] = true;
+        costSum += currCusto;
+
+        for (Edge edge : adj[currVertice]) {
+            if (!visited[edge.v]) {
+                s.insert({edge.custo, edge.v});
+            }
+        }
     }
+
+    cout << costSum << endl;
 }
 
 int main() {
     int n, m;
-    vector<Aresta> arestas;
 
     scanf("%d%d", &n, &m);
+
+    vector<vector<Edge>> adj(n);
 
     if (DEBUG) {
         cout << "N: " << n << ", M: " << m << endl;
@@ -250,19 +127,24 @@ int main() {
         int v1, v2, a, t, c;
         scanf("%d%d%d%d%d", &v1, &v2, &a, &t, &c);
 
-        arestas.push_back(Aresta(v1 - 1, v2 - 1, a, t, c));
+        adj[v1 - 1].push_back(Edge(v2 - 1, a, t, c));
+        adj[v2 - 1].push_back(Edge(v1 - 1, a, t, c));
     }
 
     if (DEBUG) {
-        cout << "Arestas:" << endl;
-        for (int i = 0; i < arestas.size(); i++) {
-            cout << "v1: " << arestas[i].v1 << ", v2: " << arestas[i].v2 << ", a: " << arestas[i].ano << ", t: " << arestas[i].tempo << ", c: " << arestas[i].custo << endl;
+        cout << "Adjacencias:" << endl;
+        for (int i = 0; i < adj.size(); i++) {
+            cout << i << ": ";
+            for (int j = 0; j < adj[i].size(); j++) {
+                cout << "v: " << adj[i][j].v << ", a: " << adj[i][j].ano << ", t: " << adj[i][j].tempo << ", c: " << adj[i][j].custo;
+            }
+            cout << endl;
         }
     }
 
-    dijkstra(0, n, arestas);
-    primAno(0, n, arestas);
-    prim(0, n, arestas);
+    dijkstra(0, n, adj);
+    primAno(0, n, adj);
+    primCusto(0, n, adj);
 
     return 0;
 };
